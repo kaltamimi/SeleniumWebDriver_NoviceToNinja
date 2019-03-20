0874afb454c766b3947d473de5f45daec6cbce19
kawthar <kawthermujahed@gmail.com>
2019-03-20 21:21:53 +0200

    wait types

--- src/main/java/usefulmethod/GenericMethods.java
+++ src/main/java/usefulmethod/GenericMethods.java
@@ -3,6 +3,8 @@ package usefulmethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -103,6 +105,42 @@ public class GenericMethods {
         }
         else {
             return false;
+        }
+    }
+
+
+    public WebElement waitForElement(By locator, int timeout){
+        WebElement element = null;
+
+        try{
+            System.out.println("Waiting for max:: " + timeout + " seconds for element to be available");
+
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+            element = wait.until(
+                    ExpectedConditions.visibilityOfElementLocated(locator));
+
+            System.out.println("Element appeared on the web page");
+        } catch(Exception e) {
+            System.out.println("Element not appeared on the web page");
+        }
+
+        return element;
+    }
+
+
+    public void clickWhenReady(By locator, int timeout){
+        WebElement element = null;
+
+        try{
+            System.out.println("Waiting for max:: " + timeout + " seconds for element to be available");
+
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+            element = wait.until(
+                    ExpectedConditions.elementToBeClickable(locator));
+
+            System.out.println("Element appeared on the web page");
+        } catch(Exception e) {
+            System.out.println("Element not appeared on the web page");
         }
     }
 }
