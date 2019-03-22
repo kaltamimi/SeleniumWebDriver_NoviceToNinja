6f8a16353faec1eae095bc0c7aaf25072779b320
kawthar <kawthermujahed@gmail.com>
2019-03-22 20:36:03 +0200

    log

--- src/main/java/letskodeit/LoggingFile.java
+++ src/main/java/letskodeit/LoggingFile.java
@@ -1,4 +1,4 @@
-package log4jsection;
+package letskodeit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,7 +8,9 @@ public class LoggingFile {
     private static final Logger log = LogManager.getLogger(LoggingFile.class.getName());
 
     public static void main(String[] args) {
+        log.trace("Trace Message Logged");
         log.debug("Debug Message Logged");
+        log.info("Info Message Logged");
         log.error("Error Message Logged");
         log.fatal("Fatal Message Logged");
     }
--- src/main/java/log4jsection/LoggingDemo.java
+++ src/main/java/log4jsection/LoggingDemo.java
@@ -1,14 +1,17 @@
 package log4jsection;
 
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LoggingDemo {
 
-    private final static Logger log = LogManager.getLogger(LoggingDemo.class.getName());
+    private static final Logger log = LogManager.getLogger(LoggingDemo.class.getName());
 
     public static void main(String[] args) {
+        log.trace("Trace Message Logged");
         log.debug("Debug Message Logged");
+        log.info("Info Message Logged");
         log.error("Error Message Logged");
         log.fatal("Fatal Message Logged");
     }
