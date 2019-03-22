1247d568eb8da983850166c31705ea170e9101c5
kawthar <kawthermujahed@gmail.com>
2019-03-22 15:25:47 +0200

    switchto

--- src/main/java/switchto/SwitchFrame.java
+++ src/main/java/switchto/SwitchFrame.java
@@ -1,4 +1,4 @@
-package calendar;
+package switchto;
 
 import org.junit.After;
 import org.junit.Before;
@@ -7,9 +7,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-public class CalendarPractice {
+public class SwitchFrame {
 
     private WebDriver driver;
     private String baseUrl;
@@ -18,29 +20,27 @@ public class CalendarPractice {
     public void setUp() throws Exception {
         System.setProperty("webdriver.gecko.driver", "E:\\Udemy tutorials\\geckodriver-v0.24.0-win64\\geckodriver.exe");
         driver = new FirefoxDriver();
-        baseUrl = "http://www.expedia.com/";
+        baseUrl = "https://learn.letskodeit.com/p/practice";
 
         // Maximize the browser's window
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     }
 
     @Test()
     public void test() throws Exception {
-        //.//div[@class='datepicker-cal-month'][position()=1]//button[text()=24]
         driver.get(baseUrl);
-        // Click flights tab
-        driver.findElement(By.xpath("//button[@id='tab-package-tab-hp']/span[@class='icons-container']")).click();
-        // Find departing field
-
-        WebElement departingField = driver.findElement(By.id("package-departing-hp-package"));
-        // Click departing field
-        departingField.click();
         Thread.sleep(3000);
-        // Find the date to be selected
-        WebElement dateToSelect = driver.findElement(By.cssSelector(".datepicker-cal .datepicker-cal-month:nth-child(4) tr:nth-of-type(5) .notranslate:nth-of-type(1) [data-year]"));
-        // Click the date
-        dateToSelect.click();
+        //switch for frame
+       // driver.switchTo().frame("courses-iframe");
+        driver.switchTo().frame(0);
+
+        WebElement searchBox = driver.findElement(By.id("search-courses"));
+        searchBox.sendKeys("python");
+
+        Thread.sleep(3000);
+        driver.switchTo().defaultContent();
+
     }
 
     @After
